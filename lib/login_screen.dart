9c51587b1fe9c3bf4ce8c4527919a6b5fd3f941c
achmadsm <achmadsfm10@gmail.com>
2025-03-23 17:50:46 +0700

    feat: Add navigation to home screen from login screen
    - Adds a button to `LoginScreen` that navigates to the home screen using `go_router`.
    - Uses `context.go(Routes.home)` to perform navigation.

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:go_router_lab/app_router.dart';
 
 // Simple login screen with a button to navigate to home
 class LoginScreen extends StatelessWidget {
@@ -6,6 +8,19 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Login Screen')));
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Login Screen'),
+            ElevatedButton(
+              onPressed: () => context.go(Routes.home), // Simulate login
+              child: Text('Login'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
