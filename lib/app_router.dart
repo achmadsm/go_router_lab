23be991decf0f2b9063af0f3330914dd31a28896
achmadsm <achmadsfm10@gmail.com>
2025-03-23 17:37:58 +0700

    feat: Define app routes as constants

---
+++ lib/app_router.dart
@@ -0,0 +1,7 @@
+// Define route paths as constants for easy reference
+class Routes {
+  static const String splash = '/';
+  static const String login = '/login';
+  static const String home = '/home';
+  static const String detail = '/detail/:id';
+}
