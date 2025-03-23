34107ddbdc7132f8668c65aa04f0a6c3165d1763
achmadsm <achmadsfm10@gmail.com>
2025-03-23 17:50:06 +0700

    feat: Implement navigation after splash screen based on login status
    - Adds a dummy login state variable `_isLogin`.
    - Implements navigation to either the home screen or login screen after a 2-second delay on the splash screen, based on `_isLogin`.
    - Uses `go_router`'s `context.go` for navigation.
    - Navigates to `Routes.home` if `_isLogin` is true, or `Routes.login` otherwise.

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:go_router_lab/app_router.dart';
 
 // Splash screen that decides navigation based on login status
 class SplashScreen extends StatefulWidget {
@@ -9,6 +11,23 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  // Dummy login state, replace with actual authentication logic
+  final bool _isLogin = false;
+
+  @override
+  void initState() {
+    Future.delayed(Duration(seconds: 2), () {
+      if (!mounted) return;
+
+      if (_isLogin) {
+        context.go(Routes.home); // Navigate to home if logged in
+      } else {
+        context.go(Routes.login); // Navigate to login if not logged in
+      }
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(body: Center(child: Text('Splash Screen')));
